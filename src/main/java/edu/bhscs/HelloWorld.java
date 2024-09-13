b941baa5dc7ddab1c219c5915de3707cd6148b9c
rortizwilson <1097097@apps.nsd.org>
2024-09-13 18:19:43 +0000

    stuff

---
+++ src/main/java/edu/bhscs/HelloWorld.java
@@ -0,0 +1,16 @@
+class HelloWorld {
+  // all code in Java sits inside a class
+
+  // every Java program needs a public main method 
+  // for the next while, ALL code you write will live inside main
+  public static void main(String[] args) {
+    // they must have a conversation together
+    System.out.println("Hello!");
+    System.out.println("Hi.");
+    System.out.println(" ");
+    System.out.println("How are you?");
+    System.out.println("I am doing well, how are you?");
+    System.out.println("");
+    System.out.println("I am just fine.");
+  }
+}
